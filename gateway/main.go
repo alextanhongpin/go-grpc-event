6c8241058c6ff923f109f563c1733b42328270e0
alextanhongpin <alextan220990@gmail.com>
2017-09-12 01:51:41 +0800

    Initial Commit

---
+++ gateway/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/golang/glog"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+
+	gw "github.com/alextanhongpin/go-engineersmy-event/proto"
+)
+
+func run() error {
+	var (
+		addr = flag.String("addr", "localhost:8080", "Address of the GRPC service")
+		port = flag.String("port", ":9090", "TCP address to listen on")
+	)
+	flag.Parse()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	if err := gw.RegisterEventServiceHandlerFromEndpoint(ctx, mux, *addr, opts); err != nil {
+		return err
+	}
+
+	log.Printf("listening to port *%s\n", *port)
+	return http.ListenAndServe(*port, mux)
+}
+
+func main() {
+	defer glog.Flush()
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
---
+++ server/main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+
+	"github.com/alextanhongpin/go-engineersmy-event/app/database"
+	pb "github.com/alextanhongpin/go-engineersmy-event/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
+
+type eventServer struct {
+	db *database.Database
+}
+
+// Event shadows the ID field to map the bson.ObjectId that is not available
+// in .proto
+type Event struct {
+	ID       bson.ObjectId `bson:"_id,omitempty"`
+	pb.Event `bson:",inline"`
+}
+
+func (s eventServer) GetEvents(ctx context.Context, msg *pb.GetEventsRequest) (*pb.GetEventsResponse, error) {
+	sess := s.db.Copy()
+	defer sess.Close()
+
+	c := s.db.Collection(sess, "events")
+
+	var tmpEvents []Event
+	if err := c.Find(bson.M{}).All(&tmpEvents); err != nil {
+		return nil, err
+	}
+
+	var events []*pb.Event
+	for _, event := range tmpEvents {
+		// Convert the objectId to string id
+		event.Id = event.ID.Hex()
+		cvt := pb.Event(event.Event)
+		events = append(events, &cvt)
+	}
+	return &pb.GetEventsResponse{
+		Data: events,
+	}, nil
+}
+
+func (s eventServer) GetEvent(ctx context.Context, msg *pb.GetEventRequest) (*pb.GetEventResponse, error) {
+	if !bson.IsObjectIdHex(msg.Id) {
+		return nil, grpc.Errorf(codes.FailedPrecondition, "Event does not exist or has been deleted")
+	}
+	sess := s.db.Copy()
+	defer sess.Close()
+
+	c := s.db.Collection(sess, "events")
+
+	var tmpEvt Event
+	if err := c.FindId(bson.ObjectIdHex(msg.Id)).One(&tmpEvt); err != nil {
+		return nil, err
+	}
+	tmpEvt.Id = tmpEvt.ID.Hex()
+	evt := &pb.GetEventResponse{
+		Data: &tmpEvt.Event,
+	}
+	return evt, nil
+}
+
+func (s eventServer) CreateEvent(ctx context.Context, msg *pb.CreateEventRequest) (*pb.CreateEventResponse, error) {
+	sess := s.db.Copy()
+	defer sess.Close()
+
+	c := s.db.Collection(sess, "events")
+
+	msg.Data.CreatedAt = time.Now().Unix()
+	msg.Data.UpdatedAt = time.Now().Unix()
+
+	if err := c.Insert(msg.Data); err != nil {
+		return nil, err
+	}
+
+	return &pb.CreateEventResponse{
+		Ok: true,
+	}, nil
+}
+
+func (s eventServer) UpdateEvent(ctx context.Context, msg *pb.UpdateEventRequest) (*pb.UpdateEventResponse, error) {
+	if !bson.IsObjectIdHex(msg.Data.Id) {
+		return nil, grpc.Errorf(codes.FailedPrecondition, "Event does not exist or has been deleted")
+	}
+	sess := s.db.Copy()
+	defer sess.Close()
+
+	c := s.db.Collection(sess, "events")
+
+	// Perform partial update
+	m := bson.M{
+		"name":       msg.Data.Name,
+		"uri":        msg.Data.Uri,
+		"start_date": msg.Data.StartDate,
+		"updated_at": time.Now().Unix(),
+	}
+
+	if len(msg.Data.Tags) != 0 {
+		m["tags"] = msg.Data.Tags
+	}
+
+	// Remove unused fields
+	for k, v := range m {
+		switch i := v.(type) {
+		case int:
+			if i == 0 {
+				delete(m, k)
+			}
+		case string:
+			if i == "" {
+				delete(m, k)
+			}
+		}
+	}
+	// Optional, use reflect to check the type
+	// v := reflect.ValueOf(x)
+	// switch v.Kind() {
+	// case reflect.Bool:
+	//     fmt.Printf("bool: %v\n", v.Bool())
+	// case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
+	//     fmt.Printf("int: %v\n", v.Int())
+	// case reflect.Uint, reflect.Uint8, reflect.Uint32, reflect.Uint64:
+	//     fmt.Printf("int: %v\n", v.Uint())
+	// case reflect.Float32, reflect.Float64:
+	//     fmt.Printf("float: %v\n", v.Float())
+	// case reflect.String:
+	//     fmt.Printf("string: %v\n", v.String())
+	// case reflect.Slice:
+	//     fmt.Printf("slice: len=%d, %v\n", v.Len(), v.Interface())
+	// case reflect.Map:
+	//     fmt.Printf("map: %v\n", v.Interface())
+	// case reflect.Chan:
+	//     fmt.Printf("chan %v\n", v.Interface())
+	// default:
+	//     fmt.Println(x)
+	// }
+	if err := c.UpdateId(bson.ObjectIdHex(msg.Data.Id), bson.M{
+		"$set": m,
+	}); err != nil {
+		return nil, err
+	}
+
+	return &pb.UpdateEventResponse{
+		Ok: true,
+	}, nil
+}
+
+func (s eventServer) DeleteEvent(ctx context.Context, msg *pb.DeleteEventRequest) (*pb.DeleteEventResponse, error) {
+	if !bson.IsObjectIdHex(msg.Id) {
+		return nil, grpc.Errorf(codes.FailedPrecondition, "Event does not exist or has been deleted")
+	}
+	sess := s.db.Copy()
+	defer sess.Close()
+
+	c := s.db.Collection(sess, "events")
+	if err := c.RemoveId(bson.ObjectIdHex(msg.Id)); err != nil {
+		return nil, err
+	}
+	return &pb.DeleteEventResponse{
+		Ok: true,
+	}, nil
+}
+
+func main() {
+	var (
+		port = flag.String("port", ":8080", "TCP port to listen on")
+	)
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", *port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	db, err := database.New()
+	if err != nil {
+		log.Fatalf("error connecting to db: %v\n", err)
+	}
+	defer db.Close()
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterEventServiceServer(grpcServer, &eventServer{
+		db: db,
+	})
+
+	log.Printf("listening to port *%s. press ctrl + c to cancel.\n", *port)
+	grpcServer.Serve(lis)
+}
