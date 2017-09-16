4098439cda0bb80d4ac2ced5978b5630dd62fe4a
alextanhongpin <alextan220990@gmail.com>
2017-09-16 14:30:28 +0800

    refactor: folder name

--- internal/jaeger/jaeger.go
+++ internal/jaeger/jaeger.go
@@ -1,39 +1,50 @@
-// package jaeger
+package hunter
 
-// import (
-// 	"time"
+import (
+	"io"
 
-// 	opentracing "github.com/opentracing/opentracing-go"
-// 	jaeger "github.com/uber/jaeger-client-go"
+	opentracing "github.com/opentracing/opentracing-go"
+	jaeger "github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/zipkin"
+)
 
-// 	"github.com/uber/jaeger-client-go/config"
-// )
+func New() (opentracing.Tracer, io.Closer) {
+	zipkinPropagator := zipkin.NewZipkinB3HTTPHeaderPropagator()
+	injector := jaeger.TracerOptions.Injector(opentracing.HTTPHeaders, zipkinPropagator)
+	extractor := jaeger.TracerOptions.Extractor(opentracing.HTTPHeaders, zipkinPropagator)
+	zipkinSharedRPCSpan := jaeger.TracerOptions.ZipkinSharedRPCSpan(true)
 
-// func New() (opentracing.Tracer, error) {
-// 	var trc opentracing.Tracer
-// 	var err error
-// 	cfg := config.Configuration{
-// 		Sampler: &config.SamplerConfig{
-// 			Type:              "const",
-// 			Param:             1,
-// 			SamplingServerURL: "localhost:5775",
-// 		},
-// 		Reporter: &config.ReporterConfig{
-// 			LogSpans:            false,
-// 			BufferFlushInterval: 1 * time.Second,
-// 		},
-// 	}
+	tracer, closer := jaeger.NewTracer(
+		"grpc_event",
+		jaeger.NewConstSampler(true),
+		jaeger.NewNullReporter(),
+		injector,
+		extractor,
+		zipkinSharedRPCSpan,
+	)
+	return tracer, closer
+}
 
-// 	tracer, _, err := cfg.New(
-// 		"grpc_jaeger",
-// 		config.Logger(jaeger.StdLogger),
-// 		// config.Observer(rpcmetrics.NewObserver(
-// 		// 	metricsFactory.Namespace("route", nil),
-// 		// 	rpcmetrics.DefaultNameNormalizer)),
-// 	)
-// 	trc = tracer.(*opentracing.Tracer)
-// 	if err != nil {
-// 		return trc, err
-// 	}
-// 	return trc, nil
+// cfg := config.Configuration{
+// 	Sampler: &config.SamplerConfig{
+// 		Type:              "const",
+// 		Param:             1,
+// 		SamplingServerURL: "localhost:5775",
+// 	},
+// 	Reporter: &config.ReporterConfig{
+// 		LogSpans:            false,
+// 		BufferFlushInterval: 1 * time.Second,
+// 	},
 // }
+
+// tracer, _, err := cfg.New(
+// 	"grpc_jaeger",
+// 	config.Logger(jaeger.StdLogger),
+// 	// config.Observer(rpcmetrics.NewObserver(
+// 	// 	metricsFactory.Namespace("route", nil),
+// 	// 	rpcmetrics.DefaultNameNormalizer)),
+// )
+// if err != nil {
+// 	return tracer, err
+// }
+// return tracer, nil
